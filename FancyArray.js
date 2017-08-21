7605beb0367c8eefed87bc28031be9ac417e4c9e
Nick Madani <nickmadani@Nicks-MacBook-Pro.local>
2017-08-21 14:40:38 -0700

    fancyArray add

---
+++ FancyArray.js
@@ -0,0 +1,8 @@
+function fancyArray(arr){
+    for (var i = 0; i < arr.length; i++){
+      console.log(i + " -> " + arr[i]);
+    }
+    
+  }
+  
+  fancyArray(["James", "Lily", "Chad", "Frank"]);
